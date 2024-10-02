c1a46d323b41392f4b597a9265bee3ee875ee34c
Fatima-Bagheri <fatim811813@gmail.com>
2024-10-02 19:07:56 +0330

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  int num1, num2, num3;
+  int res;
+  num1 = 10;
+  num2 = 10;
+  num3 = 10;
+  res = (num1 + num2 + num3) / 3;
+  
+  std::cout << "Avg: " << res;
+}
