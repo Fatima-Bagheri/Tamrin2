8647e5273ccdeae6793445319d4432e7ea435bfc
Fatima-Bagheri <fatim811813@gmail.com>
2024-10-03 12:47:12 +0330

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -5,7 +5,7 @@ int main()
 {
   int num1, num2, num3;
   int res;
-  std::cout << "Enter Numbers: "
+  std::cout << "Enter Numbers: ";
   std::cin >> num1 >> num2 >> num3;
   res = num1 + num2 + num3;
   int avg = res / 3;
